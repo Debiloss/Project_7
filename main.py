f348f6ed09dc631cd5349b6a60d9049424e54766
Sofiane Mimeche <sofiane.mimeche@gmail.com>
2023-04-11 02:14:32 +0200

    ModifPort

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ def datadrift():
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8080)
+    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
 
 # tracer la distribution conditionnelle (ceux qui remboursent ou pas) avec filtres sur variables
 # trace une ligne verticale le client que je score afin de voir sa distribution (en fonction voisins)
