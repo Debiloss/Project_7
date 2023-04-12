f936da2853a88efbe39a4f852e9deb1a79debbfa
Sofiane Mimeche <sofiane.mimeche@gmail.com>
2023-04-12 02:41:01 +0200

    Docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM python:3.10.11-slim as build
 EXPOSE 8000
-WORKDIR /API/main
-COPY ./API /API
-COPY /API/best_model.pickle ./API/best_model.pickle
-COPY /API/lgbm_shap_explainer.pickle ./API/lgbm_shap_explainer.pickle
-COPY /API/data_drift_report.html ./API/data_drift_report.html
-COPY /API/data_model_test.csv ./API/data_model_test.csv
-COPY /API/test_info.csv ./API/test_info.csv
+WORKDIR /main
+COPY / .
+COPY /best_model.pickle ./best_model.pickle
+COPY /lgbm_shap_explainer.pickle ./lgbm_shap_explainer.pickle
+COPY /data_drift_report.html ./data_drift_report.html
+COPY /data_model_test.csv ./data_model_test.csv
+COPY /test_info.csv ./test_info.csv
 RUN python3 -m pip install --upgrade pip
 RUN pip install -r requirements.txt
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+RUN apt-get update && apt-get install -y libgomp1
+CMD ["uvicorn", "main:app", "--host", "127.0.0.1", "--port", "8000"]
--- main.py
+++ main.py
@@ -17,10 +17,10 @@ app = FastAPI(
 #df_test = pd.read_csv("application_test.csv")
 
 # Load dataset modifié
-df_test_modif = pd.read_csv("data_model_test.csv")
+df_test_modif = pd.read_csv("./data_model_test.csv")
 
 # Load dataset information
-df_test_info = pd.read_csv("test_info.csv")
+df_test_info = pd.read_csv("./test_info.csv")
 
 N_CUSTOMERS = 1000
 N_NEIGHBORS = 20
@@ -67,13 +67,13 @@ col = ['NAME_CONTRACT_TYPE', 'CODE_GENDER', 'FLAG_OWN_CAR',
 test_columns = df_test_modif.columns
 
 # Load model with pickle
-model = pickle.load(open('best_model.pickle', 'rb'))
+model = pickle.load(open('./best_model.pickle', 'rb'))
 
 # Deserialize SHAP explainer
-explainer = pickle.load(open('lgbm_shap_explainer.pickle', 'rb'))
+explainer = pickle.load(open('./lgbm_shap_explainer.pickle', 'rb'))
 
 # Get datadrift html report
-drift_report = open('data_drift_report.html', 'rb')
+drift_report = open('./data_drift_report.html', 'rb')
 
 
 def prep_data(data, n_customers):
@@ -237,7 +237,7 @@ def datadrift():
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
 
 # tracer la distribution conditionnelle (ceux qui remboursent ou pas) avec filtres sur variables
 # trace une ligne verticale le client que je score afin de voir sa distribution (en fonction voisins)
