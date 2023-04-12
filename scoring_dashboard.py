2241d1751b687733fbf2c2146e4c414a80efdf2b
Sofiane Mimeche <sofiane.mimeche@gmail.com>
2023-04-12 19:40:22 +0200

    AjoutURL

--- scoring_dashboard.py
+++ scoring_dashboard.py
@@ -9,7 +9,8 @@ import requests
 import names
 import json
 
-API_URL = 'http://127.0.0.1:8000/'
+# API_URL = 'http://127.0.0.1:8000/'
+API_URL = 'https://apicreditscoring.herokuapp.com/'
 
 # Timeout for requests (connect, read)
 TIMEOUT = (5, 30)
